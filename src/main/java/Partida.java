2ac9041fbf2c26c24ca1ac7cd0e9ff75d40ff15a
FernandoCamposSegura <campossegura19@gmail.com>
2022-05-06 19:03:41 +0200

    Creando la clase Partida

---
+++ src/main/java/Partida.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Partida {
+    private String nombre;
+    private List<Jugador> listaJugadores;
+    private Baraja baraja;
+    private HashMap<String, Integer> puntuacionJugadores;
+
+    public Partida(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void addJugadores() {
+
+    }
+
+    public void jugarPartida() {
+
+    }
+
+    public void jugarRonda() {
+
+    }
+
+    public void comprobarGanadorRonda() {
+
+    }
+
+    public void comprobarGanadorPartida() {
+
+    }
+}
