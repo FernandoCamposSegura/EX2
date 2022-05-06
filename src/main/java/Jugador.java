48b86c1e3d42eebe073246e48d5a1d10d9486cf8
FernandoCamposSegura <campossegura19@gmail.com>
2022-05-06 18:46:23 +0200

    Creando la clase Jugador

---
+++ src/main/java/Jugador.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Jugador {
+
+    private String nombre;
+    private List<Carta> misCartas;
+    private int puntuacion;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void preguntarMovimiento() {
+
+    }
+
+    public void addCartar() {
+
+    }
+
+    public void imprimirCartas() {
+
+    }
+
+    public void resetearRonda() {
+
+    }
+}
